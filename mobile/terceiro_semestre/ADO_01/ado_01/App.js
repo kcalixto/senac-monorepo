9ae2066d3b33474ba95618a4a07e6dbb3de060fd
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-05-07 14:01:06 -0300

    definindo estilo do container

--- mobile/terceiro_semestre/ADO_01/ado_01/App.js
+++ mobile/terceiro_semestre/ADO_01/ado_01/App.js
@@ -1,8 +1,20 @@
-import {View} from "react-native"
+import { View } from "react-native"
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+    borderColor: "red",
+    borderWidth: 1,
+  }
+});
